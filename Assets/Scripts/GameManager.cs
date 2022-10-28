bd04015ad10a462596c26e34b71920d02f03a5b9
Glenn R. Golden <ggolden22@mac.com>
2022-10-28 17:18:12 -0400

    challenge

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,18 +3,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     public List<GameObject> targets;
     public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI gameOverText;
+    public GameObject titleScreenGameObject;
+    public Button restartButton;
+    public bool isGameActive;
 
     private int score = 0;
-    private readonly float spawnRate = 1.0f;
+    private float spawnRate = 1.0f;
 
     IEnumerator SpawnTarget()
     {
-        while (true)
+        while (isGameActive)
         {
             yield return new WaitForSeconds(spawnRate);
             int index = Random.Range(0, targets.Count);
@@ -24,10 +30,7 @@ public class GameManager : MonoBehaviour
 
     void Start()
     {
-        StartCoroutine(SpawnTarget());
 
-        score = 0;
-        UpdateScore(0);
     }
 
     // Update is called once per frame
@@ -40,5 +43,29 @@ public class GameManager : MonoBehaviour
     {
         score += scoreToAdd;
         scoreText.text = "Score: " + score;
+    }
+
+    public void GameOver()
+    {
+        gameOverText.gameObject.SetActive(true);
+        isGameActive = false;
+        restartButton.gameObject.SetActive(true);
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void StartGame(int difficulty)
+    {
+        isGameActive = true;
+        score = 0;
+        spawnRate /= difficulty;
+
+        titleScreenGameObject.SetActive(false);
+
+        StartCoroutine(SpawnTarget());
+        UpdateScore(0);
     }
 }
